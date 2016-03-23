6f0251b3f7fc44b056be535498e495b7e6cb6c19
Tobias Zwink <tobiaszwink@gmail.com>
2016-03-23 18:56:21 -0300

    Adds result page to USD currency

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,7 +1,8 @@
-var app = angular.module('myApp', []);
+var app = angular.module('cotadim', []);
     app.controller('cotacao', function($scope, $http) {
-      $http.get("http://developers.agenciaideias.com.br/cotacoes/json").then(function(result) {
-          $scope.dolar = result.data.dolar.cotacao;
-          $scope.euro  = result.data.euro.cotacao;
+      $http.get("http://api.fixer.io/latest?base=BRL").then(function(result) {
+          $scope.dolar = result.data.rates.USD;
+          $scope.euro  = result.data.rates.EUR;
+          $scope.cad   = result.data.rates.CAD;
       });
     });
